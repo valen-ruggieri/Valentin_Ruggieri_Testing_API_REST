ae66dd1705a198789fc3acf2126ad146b6f18b0e
valentin-ruggieri <valeru.251@gmail.com>
2022-09-14 20:46:19 -0300

    test by Mocha,Chai and Supertest 2

--- src/test/Mocha/testCarts/index.test.js
+++ src/test/Mocha/testCarts/index.test.js
@@ -3,29 +3,29 @@ const assert = chai.assert;
 const expect = chai.expect;
 const request = require("supertest")("http:localhost:8080");
 
-describe("funcionalidad de los metodos del store", () => {
-  it("comprobar metodo getProducts", async () => {
+describe("🛒Funcionalidad de los metodos del cart", () => {
+  it("comprobar metodo GET /cart", async () => {
     let response = await request.get("/cart");
     expect(response.status).to.eql(200);
   });
 
-  it("Comprobar metodo postProducts", async () => {
+  it("Comprobar metodo POST /cart/addproduct/:id", async () => {
     let productId = "dsfd32423";
     let response = await request.get(`/cart/addproduct/${productId}`);
     expect(response.status).to.eql(200);
   });
 
-  it("Comprobar metodo deleteProducts", async () => {
+  it("Comprobar metodo DELETE /cart/deleteproduct/:id", async () => {
     let idProduct = "63221cf29473c5801cd4bc24";
     let response = await request.get(`/cart/deleteproduct/${idProduct}`);
     expect(response.status).to.eql(200);
   });
 
-  it("Comprobar metodo deleteAll", async () => {
+  it("Comprobar metodo DELETE /cart/deleteall", async () => {
     let response = await request.get(`/cart/deleteall`);
     expect(response.status).to.eql(200);
   });
-  it("Comprobar metodo buyAll", async () => {
+  it("Comprobar metodo GET /cart/buyAll", async () => {
     let response = await request.get(`/cart/buyAll`);
     expect(response.status).to.eql(200);
   });
--- src/test/Mocha/testProducts/index.test.js
+++ src/test/Mocha/testProducts/index.test.js
@@ -3,42 +3,42 @@ const assert = chai.assert;
 const expect = chai.expect;
 const request = require("supertest")("http://localhost:8080");
 
-describe("funcionalidad de los metodos del store", () => {
-  it("comprobar metodo getProducts", async () => {
+describe("🎁Funcionalidad de los metodos del store", () => {
+  it("comprobar metodo GET /store", async () => {
     let response = await request.get("/store");
     expect(response.status).to.eql(200);
   });
 
-  // it("comprobar metodo postProducts", async () => {
-  //   let product = {
-  //     titulo: "prodPost",
-  //     precio: "667",
-  //     descripcion: "prueba supertest",
-  //     codigo: "cod999",
-  //   };
-  //   let response = await request.post("/store/addproduct").send(product)
-  //   expect(response.status).to.eql(302);
-  // });
-  // it("comprobar metodo updateProducts", async () => {
-  //   let product = {
-  //     titulo: "prodUpdate",
-  //     precio: "337",
-  //     descripcion: "prueba supertest",
-  //     codigo: "cod229",
-  //   };
-  //   let idProduct = "63221cf29473c5801cd4bc24";
-  //   let response = await request
-  //     .post(`/store/updateproduct/${idProduct}`)
-  //     .send(product);
-  //   expect(response.status).to.eql(302);
-  // });
+  it("comprobar metodo POST /store/addproduct", async () => {
+    let product = {
+      titulo: "prodPost",
+      precio: "667",
+      descripcion: "prueba supertest",
+      codigo: "cod999",
+    };
+    let response = await request.post("/store/addproduct").send(product)
+    expect(response.status).to.eql(302);
+  });
+  it("comprobar metodo PUT /store/updateproduct/:id", async () => {
+    let product = {
+      titulo: "prodUpdate",
+      precio: "337",
+      descripcion: "prueba supertest",
+      codigo: "cod229",
+    };
+    let idProduct = "63221cf29473c5801cd4bc24";
+    let response = await request
+      .post(`/store/updateproduct/${idProduct}`)
+      .send(product);
+    expect(response.status).to.eql(302);
+  });
 
-  //  it("comprobar metodo deleteProducts", async () => {
+   it("comprobar metodo DELETE /store/deleteproduct/:id", async () => {
 
-  //    let idProduct = '63221cf29473c5801cd4bc24'
-  //    let response = await request.get(`/store/deleteproduct/${idProduct}`)
-  //    expect(response.status).to.eql(302);
-  //  });
+     let idProduct = '63221cf29473c5801cd4bc24'
+     let response = await request.get(`/store/deleteproduct/${idProduct}`)
+     expect(response.status).to.eql(302);
+   });
 
    })
 
